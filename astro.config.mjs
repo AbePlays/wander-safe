cbff62fdcfe581505d42fc4ee7be78718e1b5a33
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-09-17 12:26:05 +0530

    chore: add path alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import path from 'path'
 import solidJs from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
@@ -5,4 +6,11 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components')
+      }
+    }
+  }
 })
