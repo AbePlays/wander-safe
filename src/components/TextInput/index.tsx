00f84f5db07c8356211eebb3e755962a99f1477d
Abhishek Rawat <abhi.rawat456@gmail.com>
2023-09-20 21:57:42 +0530

    feat: add form validation using valibot

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,29 +1,39 @@
-import { type JSX } from 'solid-js'
+import { Show, type JSX, splitProps } from 'solid-js'
 
 export type TextInputProps = {
+  errorMessage?: string | undefined
   label: string
 }
 
-export default function TextInput(props: TextInputProps & Omit<JSX.IntrinsicElements['input'], 'class'>) {
-  const { classList = {}, label, ...rest } = props
+export default function TextInput(props: TextInputProps & JSX.IntrinsicElements['input']) {
+  const [, newProps] = splitProps(props, ['class', 'errorMessage'])
 
   return (
-    <div class="relative">
-      <input
-        id={`${label}-input`}
-        class={`block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer ${Object.keys(
-          classList
-        )}`}
-        placeholder=" "
-        type="text"
-        {...rest}
-      />
-      <label
-        class="absolute text-sm text-gray-600 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1 pointer-events-none"
-        for={`${label}-input`}
-      >
-        {label}
-      </label>
-    </div>
+    <>
+      <div class="relative">
+        <input
+          aria-errormessage={`${props.name}-error`}
+          aria-invalid={!!props.errorMessage}
+          id={`${props.name}-input`}
+          class={`block px-2.5 pb-1.5 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-gray-600 peer ${
+            props.class
+          } ${props.errorMessage ? 'border-red-600' : null}`}
+          placeholder=" "
+          type="text"
+          {...newProps}
+        />
+        <label
+          class="absolute text-sm text-gray-600 duration-300 transform -translate-y-3 scale-75 top-1 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-1 peer-focus:scale-75 peer-focus:-translate-y-3 left-1 pointer-events-none"
+          for={`${props.name}-input`}
+        >
+          {props.label}
+        </label>
+      </div>
+      <Show when={(props.errorMessage?.length || 0) > 0}>
+        <span class="text-red-600 text-sm" id={`${props.name}-error`}>
+          {props.errorMessage}
+        </span>
+      </Show>
+    </>
   )
 }
--- src/containers/PersonalInformation/index.tsx
+++ src/containers/PersonalInformation/index.tsx
@@ -1,29 +1,70 @@
+import { createSignal } from 'solid-js'
+import { type Input, email, enumType, isoDate, length, maxLength, minLength, object, safeParse, string } from 'valibot'
+
 import Button from '@components/Button'
 import Form from '@components/Form'
 import TextInput from '@components/TextInput'
 import ArrowIcon from '@icons/ArrowIcon'
 
+const PersonalInformationSchema = object({
+  fullName: string([
+    minLength(2, 'Should have atleast 2 characters'),
+    maxLength(50, 'Cannot have more than 50 characters')
+  ]),
+  email: string([email('Invalid email address')]),
+  phone: string([length(10, 'Invalid phone number')]),
+  dob: string([isoDate()]),
+  gender: enumType(['Man', 'Woman', 'Prefer not to say'], 'Invalid gender')
+})
+
+type FormKeys = keyof Input<typeof PersonalInformationSchema>
+
+type FormType = {
+  field: Partial<Record<FormKeys, string>>
+  error: Partial<Record<FormKeys, string>>
+}
+
 export default function PersonalInformation() {
+  const [form, setForm] = createSignal<FormType>({ error: {}, field: {} })
+
+  function validateForm(e: Event) {
+    e.preventDefault()
+    const formData = Object.fromEntries(new FormData(e.target as HTMLFormElement))
+    const res = safeParse(PersonalInformationSchema, formData)
+
+    if (res.success) {
+      setForm({ error: {}, field: res.output })
+    } else {
+      const newForm: FormType = { error: {}, field: {} }
+      res.issues.forEach((issue) => {
+        const key: FormKeys = Array.isArray(issue.path) ? issue.path[0]?.key : null
+        newForm.error[key] = issue.message
+        newForm.field = issue.input
+      })
+      setForm(newForm)
+    }
+  }
+
   return (
-    <Form classList={{ 'mt-12': true }}>
+    <Form class="mt-12" onSubmit={validateForm}>
       <Form.Heading>Personal Information</Form.Heading>
 
       <div class="space-y-4 my-8">
-        <TextInput label="Full Name" />
-        <TextInput label="E-mail Address" type="email" />
-        <TextInput label="Phone Number" type="number" />
-        <TextInput label="Date of Birth" type="date" />
-        <TextInput label="Gender" />
+        <TextInput errorMessage={form().error.fullName} label="Full Name" name="fullName" />
+        <TextInput errorMessage={form().error.email} label="E-mail Address" name="email" type="email" />
+        <TextInput errorMessage={form().error.phone} label="Phone Number" name="phone" type="number" />
+        <TextInput errorMessage={form().error.dob} label="Date of Birth" name="dob" type="date" />
+        <TextInput errorMessage={form().error.gender} label="Gender" name="gender" />
       </div>
 
       <div class="flex gap-4 font-medium text-sm">
-        <Button classList={{ 'flex gap-2 items-center': true }} color="gray">
-          <ArrowIcon classList={{ 'rotate-180': true }} />
+        <Button class="flex gap-2 items-center" color="gray">
+          <ArrowIcon class="rotate-180" />
           Back
         </Button>
-        <Button classList={{ 'flex gap-2 items-center': true }}>
+        <Button class="flex gap-2 items-center" color="red" type="submit">
           Next
-          <ArrowIcon classList={{ 'text-white': true }} />
+          <ArrowIcon class="text-white" />
         </Button>
       </div>
     </Form>
